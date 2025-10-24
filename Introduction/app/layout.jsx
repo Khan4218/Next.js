fbc9247ea81d4fd4901327011f0586bdb6b5a4de
Akbar Khan <mirakber4@gmail.com>
2025-10-25 04:48:11 +0530

    Layouts

--- Introduction/app/layout.jsx
+++ Introduction/app/layout.jsx
@@ -1,7 +1,19 @@
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+     
+      <body>
+       <header>
+        <nav>
+          <ul>
+            <li>Home</li>
+            <li>add</li>
+          </ul>
+        </nav>
+       </header>
+        {children}
+      <footer>This is a footer</footer>
+      </body>
     </html>
   )
 }
