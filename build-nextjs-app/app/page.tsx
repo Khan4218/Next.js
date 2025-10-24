0031793721ae241518aabb9785e2fb49b53645b6
Akbar Khan <miraber4@gmail.com>
2025-10-24 07:51:19 +0530

    Print-Forge Home page

--- build-nextjs-app/app/page.tsx
+++ build-nextjs-app/app/page.tsx
@@ -1,3 +1,12 @@
 export default function Home() {
-  return 
+  return (
+    <main>
+      <h1>Discover what's possible with 3D printing</h1>
+      <p>Join our community of creators and explore a vast
+        library of user-submitted models.</p>
+
+      <button>Browse Models</button>
+      <img src="/home-page-hero-img.png" />
+    </main>
+  )
 }
