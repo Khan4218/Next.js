e3996720e77bc523922a444b445687b3f2f8170f
Akbar Khan <mirakber4@gmail.com>
2025-10-26 18:40:56 -0400

    Add categories navbar

--- build-nextjs-app/app/3d-models/categories/[categoryName]/page.tsx
+++ build-nextjs-app/app/3d-models/categories/[categoryName]/page.tsx
@@ -4,6 +4,5 @@ import { getCategoryBySlug } from "@/app/lib/categories"
 export default async function CategoryPage({ params }: CategoryPageProps) {
   const { categoryName } = await params
   const category = getCategoryBySlug(categoryName)
-  console.log(category)
   return <h1>{category.displayName}</h1>
 }
