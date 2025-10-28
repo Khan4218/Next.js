c81a9067b06defda66761fdb2a7da9be1f4847c0
Akbar Khan <mirakber4@gmail.com>
2025-10-28 15:19:32 +0530

    Quick Link Refractors

--- app/components/NavLink.tsx
+++ app/components/NavLink.tsx
@@ -3,11 +3,11 @@ import { NavLinkProps } from "../types"
 
 export default function NavLink({href, children}:NavLinkProps){
     return (
-    <li className="text-sm uppercase">
+  
       <Link
         href={href}
         className="px-4 py-2 text-gray-700 transition-colors rounded-md cursor-pointer hover:text-orange-accent"
       >{children}</Link>
-    </li>
+  
     )
 }
