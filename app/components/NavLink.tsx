cab7d844778072b1482ed3e1922589f93922e8f8
Akbar Khan <mirakber4@gmail.com>
2025-10-28 18:05:57 +0530

    Style Active Link

--- app/components/NavLink.tsx
+++ app/components/NavLink.tsx
@@ -1,12 +1,12 @@
 import Link from "next/link"
 import { NavLinkProps } from "../types"
 
-export default function NavLink({href, children}:NavLinkProps){
+export default function NavLink({href, children, isActive}:NavLinkProps){
     return (
-  
+    
       <Link
         href={href}
-        className="px-4 py-2 text-gray-700 transition-colors rounded-md cursor-pointer hover:text-orange-accent"
+         className={`px-4 py-2 transition-colors rounded-md cursor-pointer hover:text-orange-accent ${isActive ? "text-orange-accent" : "text-gray-700"}`}
       >{children}</Link>
   
     )
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,9 +1,13 @@
+"use client"
+
 import Link from "next/link"
 import NavLink from "./NavLink"
 import PFLogoIcon from "@/public/printforge-logo-icon.svg"
 import PFLogo from "@/public/printforge-logo.svg"
+import { usePathname } from "next/navigation"
 
 export default function Navbar() {
+  const pathName = usePathname()
   return (
     <header className="w-full bg-white">
       <nav className="flex justify-between px-6 py-4">
@@ -25,10 +29,10 @@ export default function Navbar() {
         </Link>
         <ul className="flex items-center gap-2.5">
           <li className="text-sm uppercase cursor-pointer">
-            <NavLink href="/3d-models">3D Models</NavLink>
+            <NavLink href="/3d-models" isActive={pathName === "/3d-models"} >3D Models</NavLink>
           </li>
           <li className="text-sm uppercase cursor-pointer">
-            <NavLink href="/about">About</NavLink>
+            <NavLink href="/about" isActive={pathName === "/about"} >About</NavLink>
           </li>
         </ul>
       </nav>
