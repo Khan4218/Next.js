037612d5d6c3afa4af8d74585116ac1a37218ab4
Akbar Khan <mirakber4@gmail.com>
2025-10-29 06:42:05 +0530

    Style Categories Links

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -29,7 +29,7 @@ export default function Navbar() {
         </Link>
         <ul className="flex items-center gap-2.5">
           <li className="text-sm uppercase cursor-pointer">
-            <NavLink href="/3d-models" isActive={pathName === "/3d-models"} >3D Models</NavLink>
+            <NavLink href="/3d-models" isActive={pathName.startsWith( "/3d-models")} >3D Models</NavLink>
           </li>
           <li className="text-sm uppercase cursor-pointer">
             <NavLink href="/about" isActive={pathName === "/about"} >About</NavLink>
