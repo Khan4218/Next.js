2dfc244461023d135c1c88f2c17fe64441f11cf7
Akbar Khan <mirakber4@gmail.com>
2025-10-29 13:36:22 +0530

    structured Play

--- side-project/.next/dev/server/chunks/ssr/[root-of-the-server]__1bca7b1e._.js
+++ side-project/.next/dev/server/chunks/ssr/[root-of-the-server]__1bca7b1e._.js
@@ -23,11 +23,25 @@ async function getCatFact() {
     const res = await fetch("https://catfact.ninja/fact", {
         cache: "no-store"
     });
-    const data = await res.json();
-    return data;
+    return await res.json();
 }
 async function Home() {
-    const catFact = await getCatFact();
+    /**
+ * Structured play 🛝
+ * - see what happens when you render the time stamp in dev mode
+ *   (npm run dev) vs. production mode (npm run build + npm start).
+ * - See what happens when you uncomment line 1 at the top.
+ * - Each time, before starting up the server, see if you can
+ *   guess what the behavior will be when you refresh the page;
+ *   will the timestamp update? Will it stay the same?
+ * 
+ * Note: Try to remember how we need to do things slightly differently
+ * in Scrimba. Considering downloading the code to your machine and
+ * running it locally to see what's different.
+ * 
+ * In general, be curious and play around a bit!
+ */ const catFact = await getCatFact();
+    const timestamp = new Date().toLocaleTimeString();
     return /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
         className: "page",
         children: /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("main", {
@@ -37,33 +51,46 @@ async function Home() {
                     children: "🐈‍⬛ Cat Facts 🐈"
                 }, void 0, false, {
                     fileName: "[project]/side-project/app/page.jsx",
-                    lineNumber: 16,
+                    lineNumber: 31,
                     columnNumber: 17
                 }, this),
                 /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
                     className: "fact-card",
-                    children: /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("p", {
-                        className: "fact-text",
-                        children: catFact.fact
-                    }, void 0, false, {
-                        fileName: "[project]/side-project/app/page.jsx",
-                        lineNumber: 18,
-                        columnNumber: 21
-                    }, this)
-                }, void 0, false, {
+                    children: [
+                        /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("p", {
+                            className: "timestamp",
+                            children: [
+                                "Rendered at: ",
+                                timestamp
+                            ]
+                        }, void 0, true, {
+                            fileName: "[project]/side-project/app/page.jsx",
+                            lineNumber: 33,
+                            columnNumber: 21
+                        }, this),
+                        /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("p", {
+                            className: "fact-text",
+                            children: catFact.fact
+                        }, void 0, false, {
+                            fileName: "[project]/side-project/app/page.jsx",
+                            lineNumber: 34,
+                            columnNumber: 21
+                        }, this)
+                    ]
+                }, void 0, true, {
                     fileName: "[project]/side-project/app/page.jsx",
-                    lineNumber: 17,
+                    lineNumber: 32,
                     columnNumber: 17
                 }, this)
             ]
         }, void 0, true, {
             fileName: "[project]/side-project/app/page.jsx",
-            lineNumber: 15,
+            lineNumber: 30,
             columnNumber: 13
         }, this)
     }, void 0, false, {
         fileName: "[project]/side-project/app/page.jsx",
-        lineNumber: 14,
+        lineNumber: 29,
         columnNumber: 9
     }, this);
 }
--- side-project/.next/server/app/_not-found.segments/_not-found.segment.rsc
+++ side-project/.next/server/app/_not-found.segments/_not-found.segment.rsc
@@ -1,4 +1,4 @@
 1:"$Sreact.fragment"
 2:I[39756,["/_next/static/chunks/42879de7b8087bc9.js"],"default"]
 3:I[37457,["/_next/static/chunks/42879de7b8087bc9.js"],"default"]
-0:{"buildId":"Zv4SlqjNEfJSsTxehakq7","rsc":["$","$1","c",{"children":[null,["$","$L2",null,{"parallelRouterKey":"children","template":["$","$L3",null,{}]}]]}],"loading":null,"isPartial":false}
+0:{"buildId":"u-EDdMsdvT2ppIt_skf7A","rsc":["$","$1","c",{"children":[null,["$","$L2",null,{"parallelRouterKey":"children","template":["$","$L3",null,{}]}]]}],"loading":null,"isPartial":false}
